417e83259f244ec5fd4af9a24d2a41f06c48534c
OlegDerevyanko <106957951+OlegDerevyanko@users.noreply.github.com>
2022-10-23 21:27:25 +0300

    up

---
+++ js/01-gallery.js
@@ -0,0 +1,48 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGallerySmallPictureCard(galleryItems);
+let instance;
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+galleryContainer.addEventListener("click", onGalleryImageClick);
+
+function createGallerySmallPictureCard(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+        </a>
+        </div>`;
+        })
+        .join("");
+}
+function onGalleryImageClick(e) {
+    e.preventDefault();
+
+    if (!e.target.classList.contains('gallery__image')) {
+        return;
+    };
+    let galleryImages = e.target.dataset.source;
+
+    instance = basicLightbox.create(`<img src="${galleryImages}" width="800" height="600">`);
+        instance.show();
+
+        galleryContainer.addEventListener("keydown", onEscKeyPress);
+        console.log('Esc');
+};
+        function onEscKeyPress(e) {
+      if (e.key === "Escape") {
+            instance.close(() => console.log("Esc"));
+        galleryContainer.removeEventListener("keydown", onEscKeyPress);
+      }
+    }
